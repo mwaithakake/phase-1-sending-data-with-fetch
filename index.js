aa899160d4c1ffbdffbd6bbbc491f229b00db90d
Martha <marthanyangatiwaithaka@gmail.com>
2024-04-04 20:58:33 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+function submitData (name, email) {
+const dataForm = {name, email}
+    const configObj = {
+        method : 'POST',
+        headers : {
+        "Content-Type": "application/json",
+         "Accept": "application/json",
+        } ,
+        body : JSON.stringify(dataForm)
+    }
+     return fetch ("http://localhost:3000/users"  , configObj)
+  .then(resp => resp.json())
+  .then (data => {
+    document.body.innerHTML += `<p>${data.id} </p>`;
+  })
+  .catch(error => {
+     document.body.innerHTML += `<p>${error.message}</p>`;
+  })
+  
+}
